93889286b3452f33ceeadab283edffe10e98006c
Tien Shinhan <squidwardrocs@gmail.com>
2015-10-15 14:29:39 -0400

    Finished the test application and ensured that the test was successful

--- TestApp/TestApp/frmTestApplication.Designer.vb
+++ TestApp/TestApp/frmTestApplication.Designer.vb
@@ -22,17 +22,52 @@ Partial Class frmTestApplication
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.btnTest = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblStatus = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
-        'Form1
+        'btnTest
+        '
+        Me.btnTest.Location = New System.Drawing.Point(12, 87)
+        Me.btnTest.Name = "btnTest"
+        Me.btnTest.Size = New System.Drawing.Size(75, 23)
+        Me.btnTest.TabIndex = 0
+        Me.btnTest.Text = "Test"
+        Me.btnTest.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(197, 87)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 1
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblStatus
+        '
+        Me.lblStatus.Location = New System.Drawing.Point(12, 35)
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(260, 23)
+        Me.lblStatus.TabIndex = 2
+        Me.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'frmTestApplication
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 262)
-        Me.Name = "Form1"
+        Me.ClientSize = New System.Drawing.Size(284, 126)
+        Me.Controls.Add(Me.lblStatus)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnTest)
+        Me.Name = "frmTestApplication"
         Me.Text = "Test Application"
         Me.ResumeLayout(False)
 
     End Sub
 
+    Friend WithEvents btnTest As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lblStatus As Label
 End Class
--- TestApp/TestApp/frmTestApplication.vb
+++ TestApp/TestApp/frmTestApplication.vb
@@ -1,3 +1,9 @@
 ﻿Public Class frmTestApplication
+    Private Sub btnTest_Click(sender As Object, e As EventArgs) Handles btnTest.Click
+        lblStatus.Text = "This Test application was successful"
+    End Sub
 
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
 End Class
